86f2b9d4f14f009fde18a8a08564845ef02ed60c
IvyZayn <kakapon713@outlook.com>
2023-09-05 19:23:10 +0800

    Construct the layout of the website and the routes of the header.

---
+++ client/src/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div className="p-4">
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
